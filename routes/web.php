f8ccabfba3da7b91aa2eae9f910cfc132e803d29
qwebrent <qwebrent@gmail.com>
2021-05-25 10:38:15 +0800

    Route

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'UserController', ['except' => ['show']]);
-    Route::get('survey', ['as' => 'survey.create', 'uses' => 'SurveyController@create']);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('survey', ['as' => 'survey.create', 'uses' => 'SurveyController@create']);
 });
 
